04181f275876217debc625fd9c98fd04e8ea8cab
doidea <102913254+doidea@users.noreply.github.com>
2023-02-28 05:58:42 +0800

    fix

--- nf_dns.sh
+++ nf_dns.sh
@@ -21,7 +21,7 @@ if [ $? -eq 0 ]; then
     systemctl enable dnsmasq
     rm -f /etc/resolv.conf
     echo "nameserver 127.0.0.1" > /etc/resolv.conf
-    touch /etc/dnsmasq.d/unlock.conf
+    mkdir -p /etc/dnsmasq.d/ && touch /etc/dnsmasq.d/unlock.conf
     echo "server=1.1.1.1" > /etc/dnsmasq.d/unlock.conf
     echo "server=8.8.8.8" >> /etc/dnsmasq.d/unlock.conf
     echo "server=114.114.114.114" >> /etc/dnsmasq.d/unlock.conf
